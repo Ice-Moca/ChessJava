8f71b537e8a03ec89cdda2341ef79095826ed7a8
Ice-Moca <corqjffpwhk@gmail.com>
2024-09-14 17:47:10 +0900

    Piece.java

--- src/chesswithjava/Piece.java
+++ src/chesswithjava/Piece.java
@@ -1,16 +1,15 @@
 package chesswithjava;
 
-import chesswithjava.PieceType.*;
-import chesswithjava.PlayerType.*;
-
 public abstract class Piece {
 	private final PieceType pieceType;
     private final PlayerType playerType;
+    public Position pos;
 
     //Piece 선언
-    Piece(PieceType pieceType, PlayerType playerType) {
+    Piece(PieceType pieceType, PlayerType playerType,Position pos) {
         this.pieceType = pieceType;
         this.playerType = playerType;
+        this.pos=pos;
     }
     
     //Getter method
@@ -21,27 +20,36 @@ public abstract class Piece {
     public PlayerType getPlayerType() {
         return playerType;
     }
-    
+    //Piece 입력된 자리가 자기자신의 자리인지 확인
+    public boolean Stable(Position Targetposition){
+		if(this.pos.index_x==Targetposition.index_x && this.pos.index_y==Targetposition.index_y) {
+			return false;
+		}
+		else {
+			return true;
+		}
+    }
     //Piece 이동가능한지 확인
-    public boolean moveAble(Board board, PlayerType playeTtype, Position position, Position Targetposition){
+    public boolean moveAble(Board board, PlayerType playeType, Position Targetposition){
+		
 		return false;
-    	
     }
     
     //Piece 공격가능한지 확인 
-    public boolean Attackable(Board board, PlayerType playeTtype, Position position, Position Targetposition) {
-    	return false;
+	public boolean Attackable(Board board, PlayerType playeType, Position Targetposition) {
+		
+		return false;
     }
     
     //이동 시 King이 check되는지 확인
-    public boolean CheckIfMoved(Board board, PlayerType playeTtype, Position position, Position Targetposition){
+	public boolean CheckIfMoved(Board board, PlayerType playeType, Position Targetposition){
+		
     	return false;
     }
     
     //확인 후 진짜 이동
-    public void Move(Board board, PlayerType playeTtype, Position position, Position Targetposition) {
+    public void Move(Board board, PlayerType playeType, Position Targetposition) {
     	
-    }
-    
+    }    
     
 }
